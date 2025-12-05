84d5d7717ffae2c1ab1c5afbab488a33d190b4b6
Charles Williams <charlwillia6@gmail.com>
2025-12-04 20:26:02 -0500

    Add transaction validation and history tracking for withdrawals and deposits

--- api/handlers/transactionHandler.ts
+++ api/handlers/transactionHandler.ts
@@ -1,36 +1,70 @@
 import { query } from "../utils/db";
 import { getAccount } from "./accountHandler";
+import { Account, validateWithdrawal, validateDeposit } from "../utils/transactionRules";
+import { getTodaysWithdrawnTotal } from "../utils/transactionHistory";
 
 export const withdrawal = async (accountID: string, amount: number) => {
-  const account = await getAccount(accountID);
-  account.amount -= amount;
-  const res = await query(`
+  const accountRow = await getAccount(accountID);
+  const account = accountRow as Account;
+
+  const todaysWithdrawn = await getTodaysWithdrawnTotal(accountID);
+
+  validateWithdrawal(account, amount, todaysWithdrawn);
+
+  const newAmount = account.amount - amount;
+
+  const res = await query(
+    `
     UPDATE accounts
     SET amount = $1 
-    WHERE account_number = $2`,
-    [account.amount, accountID]
+    WHERE account_number = $2
+    `,
+    [newAmount, accountID]
   );
 
   if (res.rowCount === 0) {
     throw new Error("Transaction failed");
   }
 
-  return account;
-}
+  await query(
+    `
+    INSERT INTO transactions (account_number, type, amount)
+    VALUES ($1, 'withdraw', $2)
+    `,
+    [accountID, amount]
+  );
+
+  return { ...account, amount: newAmount };
+};
 
 export const deposit = async (accountID: string, amount: number) => {
-  const account = await getAccount(accountID);
-  account.amount += amount;
-  const res = await query(`
+  const accountRow = await getAccount(accountID);
+  const account = accountRow as Account;
+
+  validateDeposit(account, amount);
+
+  const newAmount = account.amount + amount;
+
+  const res = await query(
+    `
     UPDATE accounts
     SET amount = $1 
-    WHERE account_number = $2`,
-    [account.amount, accountID]
+    WHERE account_number = $2
+    `,
+    [newAmount, accountID]
   );
 
   if (res.rowCount === 0) {
     throw new Error("Transaction failed");
   }
 
-  return account;
-}
+  await query(
+    `
+    INSERT INTO transactions (account_number, type, amount)
+    VALUES ($1, 'deposit', $2)
+    `,
+    [accountID, amount]
+  );
+
+  return { ...account, amount: newAmount };
+};
